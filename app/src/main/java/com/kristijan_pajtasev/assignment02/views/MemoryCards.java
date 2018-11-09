6a02859a462e70f161166b6da763c032fba9b92f
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2018-11-09 22:28:22 +0000

    init custom view

---
+++ app/src/main/java/com/kristijan_pajtasev/assignment02/views/MemoryCards.java
@@ -0,0 +1,20 @@
+package com.kristijan_pajtasev.assignment02.views;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class MemoryCards extends View {
+    public MemoryCards(Context context) {
+        super(context);
+    }
+
+    public MemoryCards(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MemoryCards(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
