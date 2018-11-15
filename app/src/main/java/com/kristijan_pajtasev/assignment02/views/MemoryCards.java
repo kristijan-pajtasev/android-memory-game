e439af9e5b7bee7bbc9b51c38039a1df01fe3d25
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2018-11-15 01:26:04 +0000

    init squares

--- app/src/main/java/com/kristijan_pajtasev/assignment02/views/MemoryCards.java
+++ app/src/main/java/com/kristijan_pajtasev/assignment02/views/MemoryCards.java
@@ -6,12 +6,15 @@ import android.graphics.Paint;
 import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 import java.util.ArrayList;
 
 public class MemoryCards extends View {
     private ArrayList<Rect> cardsPlaces = new ArrayList<>();
+    private Paint blue, red, green, purple, black;
 
     public MemoryCards(Context context) {
         super(context);
@@ -29,8 +32,14 @@ public class MemoryCards extends View {
     }
 
     public void initialize() {
+        black = getPaint(0xFF000000);
+        blue = getPaint(0xff0000ff);
+        green = getPaint(0xff00ff00);
+        red = getPaint(0xffff0000);
+        purple = getPaint(0xffff00ff);
+
         for (int i = 0; i < 16; i++) {
-            cardsPlaces.set(i, new Rect(100, 100, 100, 100));
+            cardsPlaces.add(new Rect(0, 0, 200, 200));
         }
 
     }
@@ -39,9 +48,35 @@ public class MemoryCards extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         for (int i = 0; i < 16; i++) {
-            Paint p = new Paint(Paint.ANTI_ALIAS_FLAG);
-            p.setColor(0xff0000);
-            canvas.drawRect(cardsPlaces.get(i), p);
+            canvas.save();
+            canvas.translate((i % 4) * 250, ((int)(i/4)) * 250);
+            canvas.drawRect(cardsPlaces.get(i), red);
         }
+    }
+
+    private Paint getPaint(int color) {
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setColor(color);
+        return paint;
+    }
+
+    public boolean onTouchEvent(MotionEvent event) {
+        int actionMasked = event.getActionMasked();
+        int pointerID = event.getPointerId(event.getActionIndex());
+
+        if (actionMasked == MotionEvent.ACTION_DOWN) {
+            float x  = event.getX(pointerID);
+            float y = event.getY(pointerID);
+            Log.d("MemoryCardView: ", "Action down event at (" + x + ", " + y + ")");
+        } else if(actionMasked == MotionEvent.ACTION_MOVE) {
+            Log.d("MemoryCardView: ", "Action move event");
+
+        } else if(actionMasked == MotionEvent.ACTION_POINTER_DOWN){
+            Log.d("MemoryCardView: ", "Action pointer down event");
+        } else if(actionMasked == MotionEvent.ACTION_POINTER_UP) {
+            Log.d("MemoryCardView: ", "Action pointer up event");
+        }
+        invalidate();
+        return super.onTouchEvent(event);
     }
 }
