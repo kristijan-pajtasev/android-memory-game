0ed622bf5fa46e9832e81cec5119176343af896c
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2018-11-21 01:25:37 +0000

    design

--- app/src/main/java/com/kristijan_pajtasev/assignment02/MainActivity.java
+++ app/src/main/java/com/kristijan_pajtasev/assignment02/MainActivity.java
@@ -4,9 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
